93b67c3ed616a73ed023f11715afe543c8dbb2c7
Jiri Brabec <brabeji@gmail.com>
2017-09-18 16:53:16 +0200

    fix: use .babelrc extend

--- features/es7.js
+++ features/es7.js
@@ -8,36 +8,9 @@ module.exports = ({ baseConfig, projectRootDirectory, isDevServer }) => {
 				{
 					test: /\.jsx?$/,
 					exclude: /node_modules/,
-					// function (a, b) {
-					// 	// console.log(a);
-					// 	const c = a.match(/node_modules/i);// || a.match(/re-app-builder/i);
-					// 	if (!c) {
-					// 		console.log(a);
-					// 		return false;
-					// 		// process.exit();
-					// 	}
-					// 	// console.log(b.length);
-					// 	// process.exit();
-					// 	return !!c.length;
-					// },
 					use: [
 						{
 							loader: 'babel-loader',
-							options: {
-								// ignore: '/node_modules/',
-								// passPerPreset: true,
-								presets: [
-									'react',
-									'es2015',
-									'stage-0'
-								],
-								plugins: [
-									'syntax-flow',
-									'transform-runtime',
-									'tcomb',
-									'transform-flow-strip-types'
-								]
-							}
 						}
 					],
 				}
